5570303e030df29695376245262c217a6cd741c8
Tambah Rizki Muhammad <37837944+Tamrizki@users.noreply.github.com>
2025-12-29 21:49:52 +0700

    assets

---
+++ lib/consts/assets.dart
@@ -0,0 +1,11 @@
+class Assets {
+  static const background = 'city_scape.jpg';
+  static const backgroundFullPath = 'assets/images/city_scape.jpg';
+  static const road = 'jalan.png';
+  static const monas = 'monas.png';
+  static const monasRotate = 'monas_rotate.png';
+
+  static const jokowiUp = 'jokowi_atas.png';
+  static const jokowiDown = 'jokowi_bawah.png';
+  static const jokowiMid = 'jokowi_depan.png';
+}
---
+++ lib/consts/consts.dart
@@ -0,0 +1,11 @@
+class Consts {
+  static const gameSpeed = 180.0;
+  static const groundHeight = 100.0;
+  static const obstacleInterval = 2.5;
+  static const playerVelocity = 300;
+  static const gravity = -100.0;
+}
+
+enum Movement { up, down, middle }
+
+enum ObstaclePotition { top, bottom }
